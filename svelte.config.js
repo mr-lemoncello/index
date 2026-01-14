af1af79865f1c1bedcf6ba2148e8238a19d75791
mr-lemoncello <vikasarino@gmail.com>
2026-01-13 23:01:57 -0800

    svelte.config.js fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   compilerOptions: {
@@ -13,7 +12,7 @@ const config = {
       fallback: '404.html'
     }),
     paths: {
-      base: '/mr-lemoncello.github.io',
+      base: '', // ← IMPORTANT
     },
   },
 };
