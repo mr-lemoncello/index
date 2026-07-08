20acf586eef9fc4eff8de02222fe36a49e776d0c
mr-lemoncello <vikasarino@gmail.com>
2026-07-07 23:57:42 -0400

    feat: added this past season's list of songs

--- src/lib/components/AudioPlayer.svelte
+++ src/lib/components/AudioPlayer.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+  import all_these_things from "$lib/assets/All These Things That I've Done.mp3";
+  import amerika from "$lib/assets/amerika.mp3";
+  import axis_of_consciousness from "$lib/assets/Axis of Consciousness.mp3";
+  import binary_groove from "$lib/assets/Binary Groove.mp3";
   import blindsided_surveillance from "$lib/assets/blindedsided_surveillance.mp3";
+  import colonnade from "$lib/assets/Colonnade.mp3";
+  import gartic_phone from "$lib/assets/Gartic_Phone.mp3";
   import crows_at_duskfall from "$lib/assets/Crows at Duskfall.mp3";
   import film_soundtrack from "$lib/assets/Flim Soundtrack.mp3";
   import for_my_own_sake from "$lib/assets/For My Own Sake.mp3";
@@ -7,11 +13,13 @@
   import garageille_o_neal from "$lib/assets/Garageille O_Neal.mp3";
   import garlic_bread_on_venus from "$lib/assets/Garlic Bread on Venus.mp3";
   import i_wash_the_sheets from "$lib/assets/I Wash The Sheets.mp3";
-  import lord_of_the_flies_chapter_one from "$lib/assets/lotf chapter 1.mp3";
+  import lord_of_the_flies_chapter_one from "$lib/assets/chapter 1.mp3";
   import monotony from "$lib/assets/Monotony.mp3";
   import no_vows_no_veins from "$lib/assets/No Vows, No Veins.mp3";
   import pain_shame_memories from "$lib/assets/Pain, Shame, Memories.mp3";
+  import quicksilver from "$lib/assets/quicksilver.mp3";
   import roll_credits from "$lib/assets/credits theme.mp3";
+  import schlorp from "$lib/assets/schlorp.mp3"
   import serenade_of_confession from "$lib/assets/Serenade of Confession.mp3";
   import spirals_and_stars from "$lib/assets/Spirals and Stars.mp3";
   import the_threads from "$lib/assets/The Threads.mp3";
@@ -20,7 +28,13 @@
   let index = $state(0);
 
   const playlist = [
+    { src: all_these_things, title: "All These Things That I've Done - Cover"},
+    { src: amerika, title: "Amerika"},
+    { src: axis_of_consciousness, title: "Axis of Consciousness"},
+    { src: binary_groove, title: "Binary Groove"},
     { src: blindsided_surveillance, title: "Blindsided Surveillance" },
+    { src: colonnade, title: "Colonnade"},
+    { src: gartic_phone, title: "Gartic Phone"},
     { src: crows_at_duskfall, title: "Crows at Duskfall" },
     { src: film_soundtrack, title: "Film Soundtrack" },
     { src: for_my_own_sake, title: "For My Own Sake" },
@@ -32,7 +46,9 @@
     { src: monotony, title: "Monotony" },
     { src: no_vows_no_veins, title: "No Vows, No Veins" },
     { src: pain_shame_memories, title: "Pain, Shame, Memories" },
+    { src: quicksilver, title: "Quicksilver"},
     { src: roll_credits, title: "Roll Credits" },
+    { src: schlorp, title: "Schlorp"},
     { src: serenade_of_confession, title: "Serenade of Confession" },
     { src: spirals_and_stars, title: "Spirals and Stars" },
     { src: the_threads, title: "The Threads" },
